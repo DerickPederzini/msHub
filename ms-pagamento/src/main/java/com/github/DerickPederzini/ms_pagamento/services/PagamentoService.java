f80fecc11d107401da6333d9796f72cbb6083611
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-24 10:55:29 -0300

    commit pagamento service

--- ms-pagamento/src/main/java/com/github/DerickPederzini/ms_pagamento/services/PagamentoService.java
+++ ms-pagamento/src/main/java/com/github/DerickPederzini/ms_pagamento/services/PagamentoService.java
@@ -1,4 +1,25 @@
 package com.github.DerickPederzini.ms_pagamento.services;
 
+
+import com.github.DerickPederzini.ms_pagamento.data.dto.PagamentoDTO;
+import com.github.DerickPederzini.ms_pagamento.entities.Pagamento;
+import com.github.DerickPederzini.ms_pagamento.repositories.PagamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
 public class PagamentoService {
+
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+
+    @Transactional(readOnly = true)
+    public List<PagamentoDTO> getAll(){
+        List<Pagamento> pagamentos = pagamentoRepository.findAll();
+        return pagamentos.stream().map(PagamentoDTO::new).toList();
+    }
+
 }
