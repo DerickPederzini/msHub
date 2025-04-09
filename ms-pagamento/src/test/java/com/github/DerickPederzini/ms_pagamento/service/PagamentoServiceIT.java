8224b5065b3ad56c601dd644c14606fdc9372e67
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-09 09:37:44 -0300

    test IT: delete pagamento

---
+++ ms-pagamento/src/test/java/com/github/DerickPederzini/ms_pagamento/service/PagamentoServiceIT.java
@@ -0,0 +1,50 @@
+package com.github.DerickPederzini.ms_pagamento.service;
+
+import com.github.DerickPederzini.ms_pagamento.repositories.PagamentoRepository;
+import com.github.DerickPederzini.ms_pagamento.services.PagamentoService;
+import com.github.DerickPederzini.ms_pagamento.services.exceptions.ResourceNotFoundException;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class PagamentoServiceIT {
+    @Autowired
+    private PagamentoService pagamentoService;
+    @Autowired
+    private PagamentoRepository repository;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalPagamentos;
+
+    @BeforeEach
+    void setup() throws Exception {
+        existingId = 1L;
+        nonExistingId = 100L;
+        countTotalPagamentos = 6L;
+    }
+
+    @Test
+    public void deletePagamentoShouldDeleteResourceWhenIdExists() {
+        pagamentoService.deletePagamento(existingId);
+        Assertions.assertEquals(countTotalPagamentos - 1, repository.count());
+    }
+
+    @Test
+    public void deletePagamentoShouldThrowExceptionWhenIdDoesNotExist(){
+        Assertions.assertThrows(ResourceNotFoundException.class,
+                () -> {
+            pagamentoService.deletePagamento(nonExistingId);
+                });
+    }
+
+
+
+
+}
